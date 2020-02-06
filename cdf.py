713bfd7aaee19e4134683f17ed4425fd02335d2f
Domenico Pontari <fairsayan@gmail.com>
2020-02-06 09:55:19 +0100

    first commit

---
+++ cdf.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('Rischio-dei-finanziamenti.csv', delimiter=";")
+df.columns = ['Anno', 'Regione', 'Tasso', 'Inutile']
+df = df.drop('Inutile', axis=1)
+
+tassoPerRegione = df.groupby(['Regione']).agg({'Tasso': 'mean'})
+tassoPerAnno = df.groupby(['Anno']).agg({'Tasso': 'mean'})
+
+plt.hist([tassoPerAnno['Tasso'], tassoPerRegione['Tasso']], bins=10, cumulative=True, alpha=.5, density=True, label=['Per anno', 'Per regioni'])
+plt.legend()
+plt.title('CDF a confronto')
+plt.show()
